7bfd10fda89f5b5c923a8481ed462644262d59ea
Gowtham <gowtham4n@gmail.com>
2021-02-05 14:04:59 +0000

    Changed error output to stderr

--- src/main.c
+++ src/main.c
@@ -54,7 +54,7 @@ int main(int argc, char ** argv)
 	/* redirect output to show results to the given output file */
 	if(freopen(outputFileName, "w", stdout) == NULL)
 	{
-		printf("Failed opening file %s\n", outputFileName);
+		fprintf(stderr, "Failed opening file: %s, error %d: %s %s\n", outputFileName, errno, strerror(errno));
 		exit(-1);
 	}
 
@@ -78,7 +78,7 @@ void verifyCommandLineFormat(int argc, char **argv, int *numSims)
 	/* verify format */
 	if(argc != 4)
 	{
-		printf("Invalid format. The correcct format is:\n./simQ <fileIn> <numSims> <fileOut>\n");
+		fprintf(stderr, "Invalid format. The correcct format is:\n./simQ <fileIn> <numSims> <fileOut>\n");
 		exit(-1);
 	}
 
@@ -88,12 +88,12 @@ void verifyCommandLineFormat(int argc, char **argv, int *numSims)
 	/* verify format */
 	if((*numSims) == 0 && strcmp(argv[2],"0"))
 	{
-		printf("Invalid format. The number of simulations must be a positive integer.\n");
+		fprintf(stderr, "Invalid format. The number of simulations must be a positive integer.\n");
 		exit(-1);
 	}
 	else if((*numSims) == 0)
 	{
-		printf("Invalid format. The correcct format is:\n./simQ <fileIn> <numSims> <fileOut>\n");
+		fprintf(stderr, "Invalid format. The correcct format is:\n./simQ <fileIn> <numSims> <fileOut>\n");
 		exit(-1);
 	}
 }
@@ -103,50 +103,43 @@ void errChecker(int err)
 {
 	if(err == -1)	
 	{
-		printf("Wrong format in input file.\n");
-		fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
+		fprintf(stderr, "Wrong format in input file.\n");
 		exit(1);
 	}
 
 	if(err == -2)
 	{
-		printf("Parameters in input file must be positive integers. (except minNewCustomers could be 0)\n");
-		fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
+		fprintf(stderr, "Parameters in input file must be positive integers. (except minNewCustomers could be 0)\n");
 		exit(1);
 	}
 
 	if(err == -3)
 	{
-		printf("Failed opening input file.\n");
-		fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
+		fprintf(stderr, "Failed opening input file, error %d: %s\n", errno, strerror(errno));
 		exit(1);
 	}
 
 	if(err == -4)
 	{
-		printf("Max value lower than min value.\n");
-		fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
+		fprintf(stderr, "Max value of limits for uniform distribution lower than min value.\n");
 		exit(1);
 	}
 
 	if(err == -5)
 	{
-		printf("Max queue length can't be less than -1. Set as -1 only for queue with no limit.\n");
-		fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
+		fprintf(stderr, "Max queue length can't be less than -1. Set as -1 only for queue with no limit.\n");
 		exit(1);
 	}
 
 	if(err == -6)
 	{
-		printf("Standard deviation can't be larger than mean.\n");
-		fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
+		fprintf(stderr, "Standard deviation can't be larger than mean.\n");
 		exit(1);
 	}
 
 	if(err == -7)
 	{
-		printf("DistributionType must be either 0 or 1. 0 for uniform & 1 for gaussian.\n");
-		fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
+		fprintf(stderr, "DistributionType must be either 0 or 1. 0 for uniform & 1 for gaussian.\n");
 		exit(1);
 	}
 }
